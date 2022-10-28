9309afe8bf7869493638795f477be048df4ea642
Bjarne Bakk <bjarne.bakk@el-watch.com>
2022-10-28 09:22:19 +0200

    new config

---
+++ init.lua
@@ -0,0 +1,13 @@
+return {
+  colorscheme = "nordfox",
+  plugins = {
+    init = {
+      { "EdenEast/nightfox.nvim" },
+    },
+  },
+  options = {
+    opt = {
+      guifont = "FiraCode Nerd Font:h12",
+    },
+  },
+}
